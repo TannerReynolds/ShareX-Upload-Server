1c24a587bd5872319d672e7b62821151119aa623
Tanner Reynolds <TannerReynolds@Outlook.com>
2019-03-06 00:06:12 -0800

    created an admin key override for auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,5 +1,6 @@
+const c = require(`${__dirname}/../config.json`)
 function auth(myKey, givenKey) {
-    if (myKey !== null && myKey && myKey !== undefined && givenKey !== myKey) {
+    if (myKey !== null && myKey && myKey !== undefined && givenKey !== myKey && givenKey !== c.admin.key) {
         return false
     } else {
         return true
