7deee0ad65203a4f209a0eaf267d1910f32791b5
Tanner Reynolds <TannerReynolds@Outlook.com>
2019-03-10 15:38:11 -0700

    fixed navbar link labels and added pm2 to install script

--- src/install.sh
+++ src/install.sh
@@ -1,8 +1,9 @@
 if which node > /dev/null
     then
-        echo "\x1b[32mNode is already installed, skipping...\x1b[0m"
+        echo "Node is already installed, skipping..."
     else
         curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
         sudo apt-get install -y nodejs
 fi
+npm i -g pm2
 npm i
