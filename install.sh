380b0b97ccac534f503c687d48eb5d47f540dcec
4xx22 <defaltnotfound@gmail.com>
2019-12-31 15:07:04 +0100

    fixing the install of ExifTool

--- install.sh
+++ install.sh
@@ -17,13 +17,13 @@ if which node > /dev/null
         curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
         sudo apt-get install -y nodejs
 fi
-(cd src && npm i -g pm2)
-(cd src && npm i)
-(wget https://iweb.dl.sourceforge.net/project/exiftool/Image-ExifTool-11.80.tar.gz)
-(tar xvf Image-ExifTool-11.80.tar.gz)
-(cd Image-ExifTool-11.80/)
-(perl Makefile.PL)
-(make)
-(make test)
-(make install)
+cd src && npm i -g pm2
+cd src && npm i
+wget https://iweb.dl.sourceforge.net/project/exiftool/Image-ExifTool-11.80.tar.gz
+tar xvf Image-ExifTool-11.80.tar.gz
+cd Image-ExifTool-11.80
+perl Makefile.PL
+make
+make test
+make install
 echo "Finished installing dependencies!"
