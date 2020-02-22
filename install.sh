719f394b1b8fb7481f1f3d5adc935eecec528d90
Nicholas S <eramsor1@gmail.com>
2020-02-22 17:55:10 +0200

    Update nodejs version
    Using Node 12, LTS

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if which node > /dev/null
     then
         echo "Node is already installed, skipping..."
     else
-        curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+        curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
         sudo apt-get install -y nodejs
 fi
 cd src && npm i -g pm2
