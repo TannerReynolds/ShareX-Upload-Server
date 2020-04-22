a2571b07ed34e218b9ed4d9e5df098b242a7b523
Tanner <treynolds4@student.yosemite.edu>
2020-04-22 00:42:13 -0700

    Update files.js

--- src/server/routes/files.js
+++ src/server/routes/files.js
@@ -247,13 +247,13 @@ function getDate(type) {
     }
     if(type.toLowerCase() === 'month') {
         const date = new Date();
-        const currentMonth = date.getMonth() + 1;
+        let currentMonth = `${date.getMonth() + 1}`;
         if(currentMonth.length === 1) currentMonth = `0${currentMonth}`
         return currentMonth;
     }
     if(type.toLowerCase() === 'day') {
         const date = new Date();
-        const currentDay = date.getDate();
+        let currentDay = `${date.getDate()}`;
         if(currentDay.length === 1) currentDay = `0${currentDay}`;
         return currentDay;
     }
